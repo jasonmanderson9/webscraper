e7061f05840edb2de841b9f8aa02ffd1b349580a
Jason Anderson <jason2009@gmail.com>
2023-09-21 16:44:03 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.yellowpages.com/search?search_terms=music+venue&geo_location_terms=oregon'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/85.0.4183.83 Safari/537.36'}
+r = requests.get(url, headers=headers)
+soup = BeautifulSoup(r.content, 'html.parser')
+
+listing = soup.find_all('div', class_='result')
+
+for item in listing:
+    name = item.find('a', class_='business-name').text
+    address = item.find('div', class_='street-address').text
+    city_state = item.find('div', class_='locality').text
+    phone = item.find('div', class_='phones phone primary').text
+    print("Name: " + name + "\n" + "Address: " + address + "\n" + "City/State: " + city_state + "\n" + "Phone: " + phone + "\n")
